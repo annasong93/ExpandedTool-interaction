d346d4fefd6684b16adf0df9036c52269e532d92
axs <axs@github.com>
2018-04-02 03:21:09 -0700

    Project 2 Boolean

--- Project 2 Boolean/coctail.js
+++ Project 2 Boolean/coctail.js
@@ -2,163 +2,137 @@
 //   $( "img" ).click(function() {
 //     $("#rect").show("slow");
 //   });
+
+// // change color use
+// $("button").click(function(){
+//     $("h1, h2, p").toggleClass("blue");
+// });
+// 点击图片 边上coctail的名字变有颜色
+// 点击coctail 背景颜色变 名字变大变颜色
+// addclass
+// cursor
+
    $(window).on('scroll', function () {
         var scrollTop = $(window).scrollTop();
-        if (scrollTop > 5) {
-            $('#head').stop().animate({height: "0px"},1000);
-            $('#title').stop().hide(1000);
+        if (scrollTop > 700) {
+            $('#head').stop().animate({height: "0px"});
+            $('#title').hide();
         }
         else {
-             $('#head').stop().animate({height: "1000px"},1000); 
+             $('#head').stop().animate({height: "100vw"},1000); 
              $('#title').show(1000);
         }
      });
 
-$(document).ready(function(){
-    $("#img1").click(function(){
-        $("#side").animate({
-            height: 'toggle'
-        }, "slow");
-    });
-});
-
-// $(document).ready(function(){
-//     $("#img1").mouseover(function(){
-//         $(this).animate({
-//           height:'50px',
-//           width: '50px',
-//           marginTop:'-100px'
-//         }, "slow");
-//     });
-// });
-
-$(document).ready(function(){
-    $("#title").scroll(function(){
-        $(this).animate({
-          height:'50px',
-          width: '50px',
-          marginTop:'-100px'
-        }, "slow");
-    });
-});
 
 $(document).ready(function() {
   $(".a").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a1").mouseover(function() {
     var p = $(this).css("background-color", "#ff6f69");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a2").mouseover(function() {
     var p = $(this).css("background-color", "#e2605a");
-    p.hide(1500).show(1500);
-      p.css("border-radius", "50%");
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a3").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
-      p.css("border-radius", "50%");
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a4").mouseover(function() {
     var p = $(this).css("background-color", "#e2605a");
-    p.hide(1500).show(1500);
-    p.css("border-radius", "50%");
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a5").mouseover(function() {
     var p = $(this).css("background-color", "#ff6f69");
-    p.hide(1500).show(1500);
-    p.css("border-radius", "50%");
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a6").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a7").mouseover(function() {
     var p = $(this).css("background-color", "#ff6f69");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a8").mouseover(function() {
     var p = $(this).css("background-color", "#f8918d");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a9").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a10").mouseover(function() {
     var p = $(this).css("background-color", "#f8918d");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a11").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a12").mouseover(function() {
     var p = $(this).css("background-color", "#f8918d");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a13").mouseover(function() {
     var p = $(this).css("background-color", "#ff6f69");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a14").mouseover(function() {
     var p = $(this).css("background-color", "#ffc7c5");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
 });
 
 $(document).ready(function() {
   $(".a15").mouseover(function() {
     var p = $(this).css("background-color", "#e2605a");
-    p.hide(1500).show(1500);
+    p.fadeOut("slow");
   });
-});
-
-
-$("img").mouseover(function() {
-    $("#rect").animate({ backgroundColor: "olive" }, "slow");
 });
